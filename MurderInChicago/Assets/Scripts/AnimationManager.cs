d0c0885c2b6e4828a065079a05b63db45e59bf81
ash7811 <ash7811@g.rit.edu>
2024-11-26 19:02:07 -0500

    added comments in anim manager

--- MurderInChicago/Assets/Scripts/AnimationManager.cs
+++ MurderInChicago/Assets/Scripts/AnimationManager.cs
@@ -25,6 +25,7 @@ public class AnimationManager : MonoBehaviour
     // A list of currently looping sprites, used primarily for emotions.
     Dictionary<string, GameObject> currentSprites = new Dictionary<string, GameObject>();
 
+    // A "queue" that holds temporary animated sprites to be played, used for multi-target spells.
     List<Tuple<string, Vector3>> animationQueue = new List<Tuple<string, Vector3>>();
 
     public int animQueueCount
@@ -41,10 +42,10 @@ public class AnimationManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Adds an animated sprite if to the dictonary.
+    /// Adds a looping animated sprite to the dictonary.
     /// </summary>
-    /// <param name="spriteType"></param>
-    /// <param name="name">Name of the character this sprite belongs to</param>
+    /// <param name="spriteType">The name of the sprite to animate.</param>
+    /// <param name="name">Name of the character this sprite belongs to.</param>
     /// <param name="position"></param>
     public void AddLoop(string spriteType, string name, Vector3 position, bool flip)
     {
@@ -55,6 +56,10 @@ public class AnimationManager : MonoBehaviour
         currentSprites.Add(name + spriteType, animSprite);
     }
 
+    /// <summary>
+    /// Deletes a looping sprite in the dictionary.
+    /// </summary>
+    /// <param name="name">The name of the sprite to delete.</param>
     public void Delete(string name)
     {
         if (currentSprites.ContainsKey(name))
@@ -64,6 +69,12 @@ public class AnimationManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Determines the type of buff or nerf passed in. 
+    /// </summary>
+    /// <param name="buffArray">The array of stat changes.</param>
+    /// <param name="isDebuff">Whether this is a buff or a debuff.</param>
+    /// <returns>The stat the buff raises, as a string.</returns>
     public string DetermineBuff(int[] buffArray, bool isDebuff)
     {
         string suffix = isDebuff ? "Nerf" : "Buff"; 
@@ -87,31 +98,17 @@ public class AnimationManager : MonoBehaviour
             case 3:
                 return "Fortitude" + suffix;
             case 4:
-                return "Fortune" + suffix;
+                return "Fortune" + suffix; 
             default:
                 return "";
         }
     }
 
-    public void Enqueue(string animName, Vector3 position)
-    {
-        animationQueue.Add(new Tuple<string, Vector3>(animName, position));
-    }
-
-    public IEnumerator PlayQueue()
-    {
-        if (animationQueue.Count == 0)
-        {
-            yield break;
-        }
-        foreach(var anim in animationQueue)
-        {
-            StartCoroutine(AnimateSprite(anim.Item1, anim.Item2));
-        }
-        yield return new WaitUntil(() => isActive == false);
-        animationQueue.Clear();
-    }
-
+    /// <summary>
+    /// Animates Multi-Target spell sprites using the queue methods.
+    /// </summary>
+    /// <param name="spellName">Name of the spell to animate.</param>
+    /// <param name="positions">All positions to animate the sprite on.</param>
     public IEnumerator AnimateMultiTarget(string spellName, Vector3[] positions)
     {
         switch (spellName) {
@@ -132,10 +129,36 @@ public class AnimationManager : MonoBehaviour
     }
 
     /// <summary>
+    /// Adds the specified animation to the "queue".
+    /// </summary>
+    public void Enqueue(string animName, Vector3 position)
+    {
+        animationQueue.Add(new Tuple<string, Vector3>(animName, position));
+    }
+
+    /// <summary>
+    /// Plays every animation in the queue at once.
+    /// </summary>
+    public IEnumerator PlayQueue()
+    {
+        if (animationQueue.Count == 0)
+        {
+            yield break;
+        }
+        foreach (var anim in animationQueue)
+        {
+            StartCoroutine(AnimateSprite(anim.Item1, anim.Item2));
+        }
+        yield return new WaitUntil(() => isActive == false);
+        animationQueue.Clear();
+    }
+
+
+    /// <summary>
     /// Animates a sprite, then deletes it.
     /// </summary>
-    /// <param name="spriteType"></param>
-    /// <param name="position"></param>
+    /// <param name="spriteType">Name of the sprite to animate.</param>
+    /// <param name="position">Where the sprite is displayed.</param>
     public IEnumerator AnimateSprite(string spriteType, Vector3 position)
     {
         if (!spriteLibrary.ContainsKey(spriteType)) yield break;
